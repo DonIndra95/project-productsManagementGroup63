0f4573f604f95a12e2478cb7bc340bbc6eaaa146
Harshjain3007 <hjain3007@gmail.com>
2022-07-27 12:52:42 +0530

    all api created

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -41,8 +41,20 @@ const createProduct=async (req,res)=>{
 
 const getProducts=async (req,res)=>{
     try {
-        
-    } catch (err) {
+       let query = req.query 
+    const product = {isDeleted:false}
+    if(query.size) product.availableSizes = query.size
+    if(query.name) product.title = query.name
+    if(query.priceGreaterThan) product.price = {$gt:query.priceGreaterThan}
+    if(query.priceLessThan) product.price = {$lt:query.priceLessThan}
+    if(query.priceGreaterThan&&query.priceLessThan) product.price = {$lt:query.priceLessThan,$gt:query.priceGreaterThan}
+    console.log(product)
+
+    const getProductDetails = await productModel.find(product).sort({price:query.priceSort})
+
+  res.status(200).send({status:true,message:getProductDetails})
+
+} catch (err) {
         return res.status(500).send({ status: false, message: err.message });
         
     }
@@ -51,15 +63,23 @@ const getProducts=async (req,res)=>{
 const getProductDetails=async (req,res)=>{
     try {
         
-    } catch (err) {
+        let productId = req.params.productId
+        let product = await productModel.findOne({ _id: productId, isDeleted: false })
+        if (!product) return res.status(404).send({ status: false, message: "No products found or product has been deleted" })
+        res.status(200).send({ status: true, message:"Success", data: product })
+} catch (err) {
         return res.status(500).send({ status: false, message: err.message });
-        
-    }
+        }
 }
 
 const updateProduct=async (req,res)=>{
     try {
-        
+
+        let data = req.body
+        let productId = req.params.productId
+        let productUpdate = await productModel.findOneAndUpdate({_id:productId,isDeleted:false},data,{new:true})
+        if(!productUpdate) return res.status(404).send({ status: false, message: "No products found or product has been deleted" })
+        res.status(201).send({ status: true, message:"Success", data: productUpdate })
     } catch (err) {
         return res.status(500).send({ status: false, message: err.message });
         
@@ -68,11 +88,15 @@ const updateProduct=async (req,res)=>{
 
 const deleteProduct=async (req,res)=>{
     try {
-        
-    } catch (err) {
+        let productId = req.params.productId
+        let product = await productModel.findOne({ _id: productId, isDeleted: false })
+        if (!product) return res.status(404).send({ status: false, message: "No products found or product has been deleted" })
+
+        let deleteProduct = await productModel.findOneAndUpdate({ _id: productId }, { $set: { isDeleted: true, deletedAt: new Date()} }, {new: true })
+        res.status(200).send({ status: true, message: 'Success', data: deleteProduct })
+} catch (err) {
         return res.status(500).send({ status: false, message: err.message });
-        
-    }
+     }
 }
 
 
